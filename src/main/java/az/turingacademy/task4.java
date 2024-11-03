f154d1519492760907c9d7d897b3d0d2e1eda49f
DilbarMirzaeva <dlbarmirza@gmail.com>
2024-11-03 10:33:20 -0800

    sum of squares

---
+++ src/main/java/az/turingacademy/task4.java
@@ -0,0 +1,17 @@
+package az.turingacademy;
+
+import java.util.Scanner;
+
+public class task4 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        long c = (long) a * a + (long) b * b;
+        System.out.println(c);
+
+
+    }
+}
+
