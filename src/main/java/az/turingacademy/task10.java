8f032387670110b0cab4d1e558fa0bc21368b280
DilbarMirzaeva <dlbarmirza@gmail.com>
2024-11-04 11:06:43 -0800

    buratino bank cash2

---
+++ src/main/java/az/turingacademy/task10.java
@@ -0,0 +1,23 @@
+package az.turingacademy;
+
+import java.util.Scanner;
+
+public class task10 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int N= sc.nextInt();
+        if(N<1 || N>12){
+            System.out.println("false");
+        }
+        else if(N>=1 && N<4){
+            System.out.println("First");
+        } else if (N>3 && N<7) {
+            System.out.println("Second");
+        } else if (N>6 && N<9) {
+            System.out.println("Third");
+        }else{
+            System.out.println("Fourth");
+        }
+    }
+}
--- src/main/java/az/turingacademy/task3.java
+++ src/main/java/az/turingacademy/task3.java
@@ -4,17 +4,19 @@ import java.util.Scanner;
 
 public class task3 {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int s=sc.nextInt();
-        int yas=0;
-        int money=1;
-        int general=0;
-
-        while(general<s){
-            yas++;
-            general+=money;
-           money=money*2+yas;
+        Scanner sc = new Scanner(System.in);
+        long S = sc.nextLong();
+        long money=0;
+        long N=0;
+        long bank=money;
+        while(true){
+            N++;
+            money=money*2+N;
+            bank+=money;
+            if(bank>=S){
+                System.out.println(N);
+                return;
+            }
         }
-        System.out.println(yas);
     }
 }
