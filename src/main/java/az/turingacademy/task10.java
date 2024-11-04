ee1c7b3acead15320543374f8606c0a8bd049b55
DilbarMirzaeva <dlbarmirza@gmail.com>
2024-11-04 11:13:02 -0800

    monthly and quarterly

--- src/main/java/az/turingacademy/task10.java
+++ src/main/java/az/turingacademy/task10.java
@@ -6,18 +6,18 @@ public class task10 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        int N= sc.nextInt();
-        if(N<1 || N>12){
+        int N = sc.nextInt();
+        if (N < 1 || N > 12) {
             System.out.println("false");
-        }
-        else if(N>=1 && N<4){
+        } else if (N >= 1 && N < 4) {
             System.out.println("First");
-        } else if (N>3 && N<7) {
+        } else if (N > 3 && N < 7) {
             System.out.println("Second");
-        } else if (N>6 && N<9) {
+        } else if (N > 6 && N < 9) {
             System.out.println("Third");
-        }else{
+        } else {
             System.out.println("Fourth");
         }
+
     }
 }
