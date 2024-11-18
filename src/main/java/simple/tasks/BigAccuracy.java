55bdb54bfc37a727c25dc65e77cd3f9fd592ae8e
DilbarMirzaeva <dlbarmirza@gmail.com>
2024-11-17 22:59:27 -0800

    BigAccuracy

---
+++ src/main/java/simple/tasks/BigAccuracy.java
@@ -0,0 +1,20 @@
+package simple.tasks;
+
+import java.util.Scanner;
+
+public class BigAccuracy {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        short m = sc.nextShort();
+        short n = sc.nextShort();
+        short k = sc.nextShort();
+        if (n == 0) {
+            System.out.println("wrong");
+        } else {
+            float s = (float) m / n;
+            System.out.printf("%." + k + "f\n", s);
+        }
+
+    }
+}
