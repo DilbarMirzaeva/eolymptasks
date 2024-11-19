c959b8989edb47341ea66d1b2bdacd9070958c5a
DilbarMirzaeva <dlbarmirza@gmail.com>
2024-11-19 09:16:36 -0800

    PainterGardener

---
+++ src/main/java/simple/tasks/PainterGardener.java
@@ -0,0 +1,15 @@
+package simple.tasks;
+
+import java.util.Scanner;
+
+public class PainterGardener {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int color=3;
+        long h=color;
+        for (int i = 0; i < N-1; i++) {
+            h*=(color-1);
+        }
+        System.out.println(h);}
+}
