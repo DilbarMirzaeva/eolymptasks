65171f613b015f2ff3a6d41d69f5d3e67f608b74
DilbarMirzaeva <dlbarmirza@gmail.com>
2024-11-19 07:13:24 -0800

    Gardener.java

--- src/main/java/simple/tasks/Gardener.java
+++ src/main/java/simple/tasks/Gardener.java
@@ -7,14 +7,13 @@ public class Gardener {
 
         Scanner sc=new Scanner(System.in);
         int N=sc.nextInt();
-        int num=N;
-        double sum=0;
-        int K=0;
-        while(sum>0.5){
-            sum=sum+(1.0/num);
-            num--;
-            K++;
-    }
-        System.out.println(N-K-1);
+        double sum=0.0;
+        int s=0;
+        while(sum<=0.5){
+            sum=sum+1.0/(N - s);
+            s++;
+        }
+        int K=N-s;
+        System.out.println(K+1);
     }
 }
