cf318afa60da9396385381841619ba5682f545a0
DilbarMirzaeva <dlbarmirza@gmail.com>
2024-12-08 07:46:52 -0800

    ValueOfVariable3

---
+++ src/main/java/simpleTasks/ValueOfVariable3.java
@@ -0,0 +1,13 @@
+package simpleTasks;
+
+import java.util.Scanner;
+
+public class ValueOfVariable3 {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+       double x=sc.nextDouble();
+        double y=((Math.pow(x,2)+3*x-4)/(2*x-3))-(x+2)/(Math.pow(x,2)-5*x+7);
+        System.out.printf("%.3f",y);
+    }
+}
